d604728dd739611d63d090cc9844cde55c2c3fbf
Luis Gustavo <luis.gustavo.dias@hotmail.com>
2020-06-06 23:47:17 -0300

    :construction_worker: frontend: update api proxy redirect

--- frontend/src/client/http.ts
+++ frontend/src/client/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from 'store';
 import { logout } from 'store/ducks/auth';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = '/api';
 
 export const publicApi = axios.create({
   baseURL: BASE_URL,
