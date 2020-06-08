69ba7899ebed2565e66cf655849014adc092d60e
Luis Gustavo <luis.gustavo.dias@hotmail.com>
2020-06-08 20:39:51 -0300

    :construction_worker: add proxy for express server

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const API_APP_NAME = process.env.API_APP_NAME;
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+const proxyMiddleware = createProxyMiddleware('/api', {
+  target: `https://${API_APP_NAME}.herokuapp.com`,
+  pathRewrite: {
+    '^/api': '/'
+  },
+  changeOrigin: true,
+});
+
+app.use('/api', proxyMiddleware);
 
 app.get('*', function (req, res) {
  res.sendFile(path.join(__dirname, 'build', 'index.html'));
