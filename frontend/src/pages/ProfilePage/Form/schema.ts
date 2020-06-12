2303d31e83beaf66e225ccca9a0fe87e310b3c93
Luis Gustavo <luis.gustavo.dias@hotmail.com>
2020-06-11 22:36:16 -0300

    :wrench: fix profile schema validation

--- frontend/src/pages/ProfilePage/Form/schema.ts
+++ frontend/src/pages/ProfilePage/Form/schema.ts
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 const schema = yup.object().shape({
   displayName: yup.string().max(60),
-  website: yup.string().max(100),
-  bio: yup.string().max(200),
+  website: yup.string().max(100).nullable(),
+  bio: yup.string().max(200).nullable(),
 });
 
 export default schema;
